85bfed3624422d95fc637978787623134aab3d1f
Glairly <62010787@kmitl.ac.th>
2021-01-14 15:12:11 +0700

    add Vue

---
+++ my-app/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card class="text-center pa-5">
+          <v-spacer></v-spacer>
+          <v-row>
+            <v-col>
+              <v-avatar size="180">
+                <v-img src="@/assets/oak.jpg"></v-img>
+              </v-avatar>
+            </v-col>
+            <v-col class="text-left">
+              <h1>Wongvarit Pancharoen</h1>
+              <h2 class="grey--text">Oak</h2>
+              <h2 class="grey--text">62010787</h2>
+            </v-col>
+          </v-row>
+          <v-card-text> </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
